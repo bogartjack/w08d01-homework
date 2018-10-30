7076b4c2619cf424d5f3306aea21997abf66ca85
Jack Bogart <bogart.jack98@gmail.com>
2018-10-30 16:11:10 -0600

    finished homework don't feel like doing step 7

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,17 +2,38 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
+	constructor(props) {
+		super(props)
+		this.state={balance:0}
+	}
   render() {
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className="balance">${this.state.balance}</div>
+        <input ref={(input)=>this.inputBox =input } type="text" placeholder="enter an amount" />
+        <input onClick={this.handleDepositClick} type="button" value="Deposit" />
+        <input onClick={this.handleWithdrawClick} type="button" value="Withdraw" />
       </div>
     )
   }
+	handleDepositClick = (e) => {
+		e.preventDefault();
+		const amount = parseInt(this.inputBox.value);
+		const newBalance = this.state.balance + amount;
+		this.setState({balance: newBalance});
+		this.inputBox.value = '';
+	}
+	handleWithdrawClick = (e) => {
+		e.preventDefault();
+		const amount = parseInt(this.inputBox.value);
+		const newBalance = this.state.balance - amount;
+		if (newBalance >0) {
+			this.setState({balance: newBalance});
+			this.inputBox.value='';
+		}
+		else this.inputBox.value='you can\'t withdraw more than you have';
+	}	
 }
 
 export default Account;
